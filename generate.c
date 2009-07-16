1f3f547773829e38f8636e2a1b5a7a316c13eced
Burke Libbey <burke@burkelibbey.org>
2009-07-16 07:36:08 -0500

    minor syntax tweaks

--- generate.c
+++ generate.c
@@ -35,9 +35,9 @@ permute_case(unsigned long long seqnum, unsigned char *phrase)
     if (!(i % WORD_SIZE) && (i != 0)) cp++; // Skip over spaces.
 
     if (seqnum & (1ULL << i)) // The i-th bit is set. Make it upper case.
-      *cp = (unsigned char) toupper((char) *cp);
+      *cp = toupper(*cp);
     else // make sure it's lower case.
-      *cp = (unsigned char) tolower((char) *cp);
+      *cp = tolower(*cp);
 
     cp++;
   }
@@ -49,15 +49,16 @@ main(int argc, char* argv[])
   unsigned char phrase[66] = "ruby ruby ruby ruby ruby ruby ruby ruby ruby ruby ruby ruby XXXXX";
   unsigned char hash[20];
   unsigned char *sfx = (phrase + SUFFIX_OFFSET);
-  unsigned long long case_perm;
-  unsigned long long suffix_perm;
+  unsigned long long case_perm = 0;
+  unsigned long long suffix_perm = 0;
   int j;
   int len = strlen((char*) phrase);
   int distance;
   int min_distance = 1000000;
   
   /* Parse Command Line for starting point (or not) */
-  switch (argc) {
+  switch (argc)
+  {
     case 4:
       suffix_perm = atoi(argv[3]);
     case 3:
@@ -82,11 +83,11 @@ main(int argc, char* argv[])
       SHA1(phrase, len, hash);
 
       distance = hamming_distance_from_goal(hash);
-      if (distance < min_distance) {
+      if (distance < min_distance)
+      {
         min_distance = distance;
-        for (j = 0; j < 20; j++) {
+        for (j = 0; j < 20; j++)
           printf("%02x", hash[j]);
-        }
         printf(" : %llu : %llu : %d : %s\n", case_perm, suffix_perm, distance, phrase);
       }
       

83c8aab4861882e3549180a4648fe3d910418201
Burke Libbey <burke@burkelibbey.org>
2009-07-16 09:45:02 -0500

    comments

--- generate.c
+++ generate.c
@@ -14,6 +14,10 @@
 
 #define WORD_SIZE 4
 
+
+// This is basically some weird math to print a base 93 number
+// into the suffix. remember 1234 = 1*10^4 + 2*10^3 + 3*10^2 + 4*10^1
+// Same deal for base 93. It's a bit terse. 
 void
 permute_suffix(long long seqnum, unsigned char *suffix)
 {
@@ -25,6 +29,10 @@ permute_suffix(long long seqnum, unsigned char *suffix)
   }
 }
 
+// So basically what we're doing here is applying a gigantic bitmask
+// to the case of a string.
+// 1101 maps to RUbY, and so on. We skip over blanks, using mod, since we
+// know they happen every 5 chars. 
 void
 permute_case(unsigned long long seqnum, unsigned char *phrase)
 {
@@ -73,28 +81,51 @@ main(int argc, char* argv[])
       suffix_perm = 0;
       set_goal("6cac827bae250971a8b1fb6e2a96676f7a077b60");
   }  
+
+  // openssl/sha.h uses a data structure something like this:
+  // 
+  // struct SHA_CTX {
+  //   SHA_LONG lots, of, them;
+  //   SHA_LONG data[16];
+  // };
+  //
+  // We can calculate the hash incrementally. The case permutation
+  // very rarely changes, so we'll calculate the hash of that,
+  // then copy the resulting SHA_CTX for each suffix permutation,
+  // and feed in the suffix. This is much more efficient than
+  // rehashing the phrase portion each time the suffix changes
+  // ~42% performance boost.
   
   for (/* case_perm */; case_perm < CASE_SPACE; ++case_perm)
   {
     permute_case(case_perm, phrase);
 
+    // Initialize a SHA_CTX and prime it with the case permutation.
     SHA1_Init(casectx);
     SHA1_Update(casectx, phrase, strlen((char*) phrase));
     
     for (/* suffix_perm */; suffix_perm < SUFFIX_SPACE; ++suffix_perm)
     {
+      // Update sfx with the next suffix.
       permute_suffix(suffix_perm, sfx);
 
+      // Copy the SHA_CTX containing the relevant case permutation hash,
+      // and append the suffix, then pull out the final hash.
       memcpy(suffctx, casectx, sizeof(SHA_CTX));
       memcpy(suffctx->data, casectx->data, sizeof(SHA_LONG)*SHA_LBLOCK);
       SHA1_Update(suffctx, sfx, 5);
       SHA1_Final(hash,suffctx);
 
+      // If this is a new best, print 'er out.
       distance = hamming_distance_from_goal(hash);
       if (distance < min_distance)
       {
         min_distance = distance;
+        // So it's worth noting that openssl/sha.h returns a strange format.
+        // You get an array of unsigned chars, each containing the binary representation
+        // of two hex digits, so eg. char 35 is 00010011 or "13" in hex.
         for (j = 0; j < 20; j++)
+          // Hex representation makes a lot more sense to humans.
           printf("%02x", hash[j]);
         printf(" : %llu : %llu : %d : %s%s\n", case_perm, suffix_perm, distance, phrase, sfx);
       }
